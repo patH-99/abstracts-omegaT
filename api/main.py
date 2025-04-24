91a8a6e40c3d37c0ed5578a2a8a7c4ba6d4160f1
patH-99 <165496075+patH-99@users.noreply.github.com>
2025-04-24 12:21:24 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -11,19 +11,18 @@ def read_docx(file_path):
 
 
 # Load the reference and candidate .docx files
-reference_file = '/Users/patrikhrabanek/OmegaT_translations/EN-SR_corpus/target/recipe_1.docx'  # Path to the reference .docx file
-candidate_file = '/Users/patrikhrabanek/OmegaT_translations/EN-SR_corpus/target/recipe_1_copy.docx'  # Path to the candidate .docx file
+reference_file = 'path_to_file'  # Path to the reference .docx file
+candidate_file = 'path_to_file'  # Path to the candidate .docx file
 
 reference_text = read_docx(reference_file)
 candidate_text = read_docx(candidate_file)
 
-# Print the content to verify
 print("Reference Text:", reference_text)
 print("Candidate Text:", candidate_text)
 
 import sacrebleu
 
-# Calculate BLEU score using sacrebleu
+# Calculating the BLEU score using sacrebleu
 bleu = sacrebleu.corpus_bleu(candidate_text, [reference_text])
 
 print(f"BLEU score: {bleu.score}")
